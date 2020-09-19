573f2cb5fd919e6855f5e893844e693a10c15854
Lucas (Fptbb) <lucas@fptbb.com>
2020-09-19 20:01:20 -0300

    Auto Push

--- src/templates/tag/tag.js
+++ src/templates/tag/tag.js
@@ -2,7 +2,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
-import Img from 'gatsby-image'
 /* App imports */
 import Layout from '../../components/layout'
 import SEO from '../../components/seo'
