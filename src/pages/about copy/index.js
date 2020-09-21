a9a96ac7625bef62d2087940684542d8f8f220f5
Lucas (Fptbb) <lucas@fptbb.com>
2020-09-21 11:31:24 -0300

    Auto Push

---
+++ src/pages/about copy/index.js
@@ -0,0 +1,163 @@
+/* Vendor imports */
+import React from 'react'
+import PropTypes from 'prop-types'
+import { graphql } from 'gatsby'
+import Img from 'gatsby-image'
+/* App imports */
+import Layout from '../../components/layout'
+import SEO from '../../components/seo'
+import Utils from '../../utils'
+import style from './index.module.less'
+
+export const aboutPropTypes = {
+  data: PropTypes.shape({
+    profilePhoto: PropTypes.shape({
+      childImageSharp: PropTypes.shape({
+        fluid: PropTypes.object.isRequired,
+      }).isRequired,
+    }).isRequired,
+    flagIt: PropTypes.shape({
+      childImageSharp: PropTypes.shape({
+        fixed: PropTypes.object.isRequired,
+      }),
+    }),
+    flagEn: PropTypes.shape({
+      childImageSharp: PropTypes.shape({
+        fixed: PropTypes.object.isRequired,
+      }),
+    }),
+    skillIcons: PropTypes.object.isRequired,
+    toolIcons: PropTypes.object.isRequired,
+  }),
+}
+
+class About extends React.Component {
+  static propTypes = aboutPropTypes
+
+  render() {
+    let { profilePhoto, flagIt, skillIcons, toolIcons } = this.props.data
+    return (
+      <Layout>
+        <SEO
+          title="Sobre"
+          description="Um resumo basico sobre esse blog"
+          path="about"
+        />
+        <div className={style.container}>
+          <div className={style.photo}>
+            <Img fluid={profilePhoto.childImageSharp.fluid} />
+          </div>
+          <div className={style.content}>
+            <h1>Olá, eu sou o Fptbb</h1>
+            <h2>Desenvolvedor Backend</h2>
+            <p>For the English version click here</p>
+            <a href={Utils.resolvePageUrl('../', 'en', 'about')}>
+              <Img
+                fixed={flagIt.childImageSharp.fixed}
+                style={{ display: 'block', margin: 'auto' }}
+              />
+            </a>
+            <p>
+            Esse blog foi criado para publicações minhas, de meus projetos, ideias, estudos, etc, usando Gatsby, e hospedado no Github Pages, tambem usei um template como base, e modifiquei-o, seguem os links do template framework e ferramentas usadas.<br/>
+              <a href='https://pages.github.com/'>Github Pages</a><br/>
+              <a href='https://www.gatsbyjs.com/'>Gatsby</a><br/>
+              <a href='https://github.com/lgcolella/gatsby-starter-developer-blog'>Template</a>
+            </p>
+          </div>
+        </div>
+      </Layout>
+    )
+  }
+}
+
+export const imageListPropTypes = {
+  edges: PropTypes.arrayOf(
+    PropTypes.shape({
+      node: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        childImageSharp: PropTypes.shape({
+          fixed: PropTypes.object.isRequired,
+        }).isRequired,
+      }).isRequired,
+    })
+  ).isRequired,
+}
+
+class ImageList extends React.Component {
+  static propTypes = imageListPropTypes
+
+  render = () => (
+    <div className={style.iconsContainer}>
+      {this.props.edges
+        .sort((edgeA, edgeB) =>
+          edgeA.node.name.toLowerCase() > edgeB.node.name.toLowerCase() ? 1 : -1
+        )
+        .map(({ node: { name, childImageSharp } }) => (
+          <div className={style.iconWrapper} key={name}>
+            <Img
+              fixed={childImageSharp.fixed}
+              alt={name + ' logo'}
+              title={name}
+            />
+            <label>
+              {iconsNameMap[name] ? iconsNameMap[name] : Utils.capitalize(name)}
+            </label>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+export const query = graphql`
+  {
+    profilePhoto: file(name: { eq: "profile-photo" }) {
+      childImageSharp {
+        fluid(maxWidth: 800) {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+    flagIt: file(name: { eq: "flag-it" }) {
+      childImageSharp {
+        fixed(width: 50) {
+          ...GatsbyImageSharpFixed_tracedSVG
+        }
+      }
+    }
+    skillIcons: allFile(filter: { dir: { regex: "/about/skills$/" } }) {
+      edges {
+        node {
+          name
+          childImageSharp {
+            fixed(width: 50) {
+              ...GatsbyImageSharpFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+    toolIcons: allFile(filter: { dir: { regex: "/about/tools$/" } }) {
+      edges {
+        node {
+          name
+          childImageSharp {
+            fixed(width: 50) {
+              ...GatsbyImageSharpFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+// Use to set specific icons names
+export const iconsNameMap = {
+  css: 'CSS',
+  html: 'HTML',
+  jquery: 'JQuery',
+  nodejs: 'Node.js',
+  vuejs: 'Vue.js',
+  gruntjs: 'Grunt.js',
+}
+
+export default About
